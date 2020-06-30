322353086e2f879a55d55ac2a8ac3ace01a028ae
VIPWorkSpace <vipworkspace20@gmail.com>
2020-06-30 13:08:52 +0700

    admin ok

--- adminpanels/src/main/java/com/workspace/adminpanels/MainActivity.java
+++ adminpanels/src/main/java/com/workspace/adminpanels/MainActivity.java
@@ -1,26 +1,35 @@
 package com.workspace.adminpanels;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.workspace.adminpanels.Activity.DataAddMenuActivity;
 import com.workspace.adminpanels.Activity.DataMenuActivity;
 import com.workspace.adminpanels.Activity.DataTransaksi;
 import com.workspace.adminpanels.Activity.DataUserActivity;
+import com.workspace.adminpanels.Model.mainModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private GridLayout gridDashboard;
     private TextView mUsername;
     private DatabaseReference dRef;
-    String username = "";
+    List<mainModel> mainList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +40,24 @@ public class MainActivity extends AppCompatActivity {
         gridDashboard = findViewById(R.id.gridL);
         setSingleEvent(gridDashboard);
 
+        dRef = FirebaseDatabase.getInstance().getReference("Data").child("Admin");
+        dRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    String surenames = ds.getKey();
+                    if (dataSnapshot.exists()){
+                        mUsername.setText(dataSnapshot.child(surenames).child("surename").getValue().toString());
+                    }
+                    Log.d("NAME", surenames);
+                }
+            }
 
-    }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-    private void retrieveAdmin() {
+            }
+        });
     }
 
     private void setSingleEvent(GridLayout gridDashboard) {
