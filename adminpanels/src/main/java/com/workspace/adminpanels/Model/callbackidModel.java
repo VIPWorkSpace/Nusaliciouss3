877578ba5654c2b76e9c7e174d7ae0c8b5ddd079
VIPWorkSpace <vipworkspace20@gmail.com>
2020-07-01 16:33:04 +0700

    PUSINGGG CUK

---
+++ adminpanels/src/main/java/com/workspace/adminpanels/Model/callbackidModel.java
@@ -0,0 +1,22 @@
+package com.workspace.adminpanels.Model;
+
+public class callbackidModel {
+
+
+    public String key;
+
+    public callbackidModel(String key) {
+        this.key = key;
+    }
+
+    public callbackidModel() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
