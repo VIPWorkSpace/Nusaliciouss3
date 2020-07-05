76ba433e01a2afed5def53f2368e98d245ecc1a0
VIPWorkSpace <vipworkspace20@gmail.com>
2020-07-05 16:54:49 +0700

    Income Transaksi

--- adminpanels/src/main/java/com/workspace/adminpanels/Activity/DataIncome.java
+++ adminpanels/src/main/java/com/workspace/adminpanels/Activity/DataIncome.java
@@ -1,14 +1,14 @@
 package com.workspace.adminpanels.Activity;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,9 +16,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.workspace.adminpanels.Adapter.incomeAdapter;
-import com.workspace.adminpanels.Model.ChartModel;
 import com.workspace.adminpanels.Model.IncomeModel;
-import com.workspace.adminpanels.Model.dataPesanModel;
 import com.workspace.adminpanels.R;
 
 import java.util.ArrayList;
