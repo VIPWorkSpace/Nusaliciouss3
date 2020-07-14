660667af5861fb33a684356bda90380bc8486a7f
VIPWorkSpace <vipworkspace20@gmail.com>
2020-07-14 19:43:23 +0700

    Finish all

--- adminpanels/src/main/java/com/workspace/adminpanels/Fragments/PembayaranFragment.java
+++ adminpanels/src/main/java/com/workspace/adminpanels/Fragments/PembayaranFragment.java
@@ -1,5 +1,7 @@
 package com.workspace.adminpanels.Fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,10 +35,12 @@ import java.util.ArrayList;
 
 public class PembayaranFragment extends Fragment {
 
+    public static final String EXTRA_UNIX = "extra_unix";
     RecyclerView rvPembayaran;
     ArrayList<pembayaranModel> pembayaranList;
     DatabaseReference dbRef;
     pembayaranAdapter adapter;
+    public String uid;
 
     public PembayaranFragment() {
         // Required empty public constructor
@@ -46,14 +51,16 @@ public class PembayaranFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_pembayaran, container, false);
 
-
-        rvPembayaran = v.findViewById(R.id.rvd_pembayaran);
+        init(v);
         //get Data Firebase
         retrieveData();
+        retrieveIntent();
         //Recycler Setting
         recyclerSettings();
-
         return v;
+    }
+    private void init(View v) {
+        rvPembayaran = v.findViewById(R.id.rvd_pembayaran);
     }
     private void recyclerSettings(){
         rvPembayaran.setHasFixedSize(true);
@@ -62,29 +69,28 @@ public class PembayaranFragment extends Fragment {
         RecyclerView.ItemDecoration divider = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
         rvPembayaran.addItemDecoration(divider);
     }
+    private void retrieveIntent() {
+        Intent intent = ((Activity) getContext()).getIntent();
+        String unix = intent.getStringExtra(EXTRA_UNIX);;
+        uid = unix;
+    }
     private void retrieveData(){
+
         dbRef = FirebaseDatabase.getInstance().getReference("Data").child("Transaksi");
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                        String mPembayaran = ds.getKey();
-
-                        for (DataSnapshot dSnap : dataSnapshot.child(mPembayaran).child("Pembayaran").getChildren()) {
-                            String mPembayaran1 = dSnap.getKey();
-                            pembayaranModel pembayaranMod = dSnap.getValue(pembayaranModel.class);
-                            pembayaranList.add(pembayaranMod);
-                        }
-                        adapter = new pembayaranAdapter(pembayaranList, getContext());
-                        rvPembayaran.setAdapter(adapter);
-                    }
-
-
+                pembayaranList.clear();
+                for (DataSnapshot ds : dataSnapshot.child(uid).child("Pembayaran").getChildren()) {
+                    pembayaranModel pembayaranMod = ds.getValue(pembayaranModel.class);
+                    pembayaranList.add(pembayaranMod);
+                }
+                adapter = new pembayaranAdapter(pembayaranList, getContext());
+                rvPembayaran.setAdapter(adapter);
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                Toast.makeText(getContext(), "Error in : " + databaseError, Toast.LENGTH_SHORT).show();
             }
         });
     }
